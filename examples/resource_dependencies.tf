09159e931905506cb37ed7c65c847337f8db5139
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-06-25 14:55:52 +0100

    Add missing attribute

--- examples/resource_dependencies.tf
+++ examples/resource_dependencies.tf
@@ -32,6 +32,7 @@ module "service_network" {
   resource_location             = module.resource_group.location
   resource_group_name           = module.resource_group.name
   resource_environment          = var.service_environment
+  resource_deployment           = var.service_deployment
   resource_address_space        = var.resource_address_space
   resource_dns_servers          = var.resource_dns_servers
   resource_network_subnet_count = var.resource_network_interface_count
